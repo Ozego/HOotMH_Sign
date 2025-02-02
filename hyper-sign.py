0979def77cd95823bcf0106728490499fc54ecaa
Es <es@local.net>
2025-02-02 19:55:02 +0700

    _

--- hyper-sign.py
+++ hyper-sign.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_signature(width, height, max_iter):
+def hypershpere_signature(width, height, max_iter):
     # Grid
     x, y = np.linspace(-8,8,width), np.linspace(-8,8,width)
     X, Y = np.meshgrid(x,y)
@@ -10,13 +10,13 @@ def generate_signature(width, height, max_iter):
     Phi,phi=(_:=5**.5*.5)+.5,_-.5
     for i in range(max_iter):
         mask = np.abs(Z) <= 1
-        if mask.any():
-            Z+=Z*(0+1j*phi)+np.sqrt(5)
         output+=mask
+        Z+=Z*(0+1j*phi)+np.sqrt(5)
+        Z=np.abs(Z)
     return np.clip(output,0,1)
 
 width,height=512,512
-signature=generate_signature(width,height,69)
+signature=hypershpere_signature(width,height,69)
 plt.figure(figsize=(width/100,height/100),dpi=100)
 plt.axis('off')
 plt.imshow(signature,extent=(-1,1,-1,1))
