c9f066d2d76bfba2fa235d25c0a43a73bb380ae3
Es <es@local.net>
2025-02-02 09:27:05 +0700

    _

--- hyper-sign.py
+++ hyper-sign.py
@@ -6,20 +6,16 @@ def generate_signature(width, height, max_iter):
     Generate a self-rewriting combinatorial graph's discrete 2D signature using spherical harmonics.
     """
     # Grid
-    x, y = np.linspace(-3,3,width), np.linspace(-3,3,width)
+    x, y = np.linspace(-1,1,width), np.linspace(-1,1,width)
     X, Y = np.meshgrid(x,y)
     Z = X + 1j * Y
     output = np.zeros(Z.shape, dtype=float)
 
     for i in range(max_iter):
         mask = np.abs(Z) <= 1
-        Z[mask]=Z[mask]**2 +complex(.69,-.69)
         if mask.any():
-            factor = np.exp(-i/max_iter)
-            pattern = np.cos(np.linspace(0,np.pi,height))[:,None]*np.sin(np.linspace(0,np.pi,width))
-            pattern = (pattern-0.5)*factor
-            Z+=pattern
-        output+=mask
+            Z+=np.sin(np.linspace(-np.pi,np.pi,height))[:,None]*-np.cos(np.linspace(-np.pi,np.pi,width))
+        output+=mask/max_iter
 
 
     return np.clip(output,0,1)
