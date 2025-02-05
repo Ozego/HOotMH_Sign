5ff8474f6df47a9b468f05738998e58e8fe69e41
Es <es@local.net>
2025-02-05 14:51:16 +0700

    _

--- hyper-sign.py
+++ hyper-sign.py
@@ -1,8 +1,5 @@
-from os import error
-from scipy.stats import truncnorm
 import numpy as np
 import matplotlib.pyplot as plt
-import hashlib
 
 Phi,phi=( _ := 5 ** .5 * .5) + .5, _ - .5
 
@@ -11,21 +8,26 @@ Complex imaginary fractal tentacles of rotational reflected hyperspheres
 """
 def hypershpere_sign(width,height,max_iter):
     # Grid
-    x, y = np.linspace(-8, 8, width), np.linspace(-8, 8, height)
+    x, y = np.linspace(-4, 4, width), np.linspace(-4, 4, height)
     X, Y = np.meshgrid(x, y)
     Z = X + 1j * Y
-    r = 6
-    zeta = (np.pi/( .5 * r)) * np.floor( .5 * r * np.angle(Z)/np.pi + .5 * r +.5)  
-    spin = (np.sin(zeta)+np.cos(zeta)*1j)
-    Z *= spin
     output = np.zeros(Z.shape, dtype = float)
-    theta = np.pi * .25
-    for i in range(max_iter):
-        output += 1 - np.abs(Z) > 0
-        scalar = Phi
-        Z *= scalar
-        Z += (scalar + 1) * 1j 
-        Z *= np.cos(theta) + np.sin(theta) * 1j
+    for i in range (2):
+        Z = X + 1j * Y
+        r = 7
+        offset = -np.pi/r*2
+        zeta = (np.pi/( .5 * r)) * np.floor( .5 * r * np.angle(Z)/np.pi + .5 * r +.5)
+        spin = (np.sin(zeta)+np.cos(zeta)*1j)
+        Z *= spin
+        if (i==1):
+            Z *= np.cos(offset)+np.sin(offset)*1j 
+        theta = np.pi * .25
+        for j in range(max_iter):
+            output += 1 - np.abs(Z) > 0
+            scalar = Phi
+            Z *= scalar
+            Z += (scalar + 1) * 1j 
+            Z *= np.cos(theta) + np.sin(theta) * 1j
 
     return np.clip(output, 0, 1)
 
